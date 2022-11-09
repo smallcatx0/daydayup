a237e2c7839387c813d7a83744a1802afd2d212f
smallcatx0 <1312308948@qq.com>
2022-11-09 11:22:03 +0800

    水一题

--- leetcode_go/896_test.go
+++ leetcode_go/896_test.go
@@ -9,14 +9,28 @@ func isMonotonic(nums []int) bool {
 	if len(nums) <= 2 {
 		return true
 	}
-	for i := 2; i < len(nums); i++ {
-
+	op := 0
+	for i := 1; i < len(nums); i++ {
+		// 先判断单调方向
+		if op == 0 {
+			if nums[i] > nums[i-1] {
+				op = 1
+			}
+			if nums[i] < nums[i-1] {
+				op = -1
+			}
+		} else {
+			fmt.Println(nums[i] - nums[i-1]*op)
+			if (nums[i]-nums[i-1])*op < 0 {
+				return false
+			}
+		}
 	}
-	return false
+	return true
 }
 
 func Test_896(t *testing.T) {
-	nums := []int{1, 2, 3}
+	nums := []int{5, 3, 2, 4, 1}
 	ret := isMonotonic(nums)
 	fmt.Println(ret)
 }
