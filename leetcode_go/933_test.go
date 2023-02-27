3a281ab7f773608bf301f2fa1739651364ce5db6
smallcatx0 <1312308948@qq.com>
2023-02-27 20:24:41 +0800

    喜加二

--- leetcode_go/933_test.go
+++ leetcode_go/933_test.go
@@ -19,14 +19,16 @@ func Constructor() RecentCounter {
 
 func (this *RecentCounter) Ping(t int) int {
 	// 入队 (计数+)
-	top := len(this.q)
-	if this.q[top][0] == t {
+	top := len(this.q) - 1
+	if top > 0 && this.q[top][0] == t {
 		this.q[top][1] += 1
+	} else {
+		this.q = append(this.q, [2]int{t, 1})
 	}
 	this.count++
 	// 计算淘汰(计数-)
 	i := 0
-	for ; t-this.q[i][0] >= 3000; i++ {
+	for ; t-this.q[i][0] > 3000; i++ {
 		this.count -= this.q[i][1]
 	}
 	this.q = this.q[i:]
@@ -37,5 +39,8 @@ func (this *RecentCounter) Ping(t int) int {
 func Test_933(t *testing.T) {
 	q := Constructor()
 	fmt.Println(q.Ping(1))
+	fmt.Println(q.Ping(100))
+	fmt.Println(q.Ping(3001))
+	fmt.Println(q.Ping(3002))
 
 }
---
+++ leetcode_go/938_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// 中序遍历 递增有序
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	if root.Val > high {
+		return rangeSumBST(root.Left, low, high)
+	} else if root.Val < low {
+		return rangeSumBST(root.Right, low, high)
+	} else {
+		return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+	}
+}
